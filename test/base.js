6c11e83b9ed4e769ee167869146b721b0ad61ab2
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-08-31 23:19:03 -0700

    abs path to ~/.ssh

--- test/base.js
+++ test/base.js
@@ -1,3 +1,5 @@
+// ONLY USE ON CI BECAUSE IT MESSES UP `authorized_keys`
+
 var SSH = require('../'),
     exec = require('child_process').exec;
 
@@ -29,6 +31,7 @@ describe('ssh-kit', function() {
 
     it('connects to ssh', function(done) {
         this.ssh.set('host', 'localhost');
+        // relative `~/.ssh` does not work on travis
         this.ssh.set('sshKey', '/home/travis/.ssh/ssh_kit_test_key');
         this.ssh.exec('ls');
         this.ssh.on('finish', done);
