80018af511b77876aeb1473ed86e76eb2db3ca19
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-08-31 22:33:53 -0700

    id_rsa

--- test/base.js
+++ test/base.js
@@ -21,16 +21,22 @@ describe('ssh-kit', function() {
         run('pwd');
         run('whoami');
         run('cd ~ && pwd');
-        run('cp ' + dir + '/ssh_kit_test_key /home/travis/.ssh/');
-        run('chmod 600 /home/travis/.ssh/ssh_kit_test_key');
-        run('cat ' + dir + '/ssh_kit_test_key.pub >> /home/travis/.ssh/authorized_keys');
-        run('chmod 600 /home/travis/.ssh/authorized_keys');
-        run('ls -la ~/.ssh');
 
-        this.ssh.set('host', 'localhost');
-        this.ssh.set('sshKey', dir + '/ssh_kit_test_key');
-        this.ssh.exec('ls');
-        this.ssh.on('finish', done);
+        run('cp ' + dir + '/ssh_kit_test_key /home/travis/.ssh/id_rsa');
+        run('chmod 600 /home/travis/.ssh/id_rsa');
+
+        run('cat ' + dir + '/ssh_kit_test_key.pub >> /home/travis/.ssh/authorized_keys');
+
+        run('chmod 600 /home/travis/.ssh/authorized_keys');
+
+        run('ls -la /home/travis/.ssh');
+
+        setTimeout(function() {
+            this.ssh.set('host', 'localhost');
+            this.ssh.set('sshKey', dir + '/ssh_kit_test_key');
+            this.ssh.exec('ls');
+            this.ssh.on('finish', done);
+        }, 500);
 
     });
 });
