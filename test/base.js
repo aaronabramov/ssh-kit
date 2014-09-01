49836e00deb8858ca44c880a0d8bda3ad6c5cf12
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-08-31 23:09:21 -0700

    remove after hook

--- test/base.js
+++ test/base.js
@@ -1,24 +1,12 @@
 var SSH = require('../'),
     exec = require('child_process').exec;
 
-function run(cmd, done) {
-    exec(cmd, function(err, stdout, stderr) {
-        err && done(err);
-        console.log(cmd);
-        console.log('err', err);
-        console.log('stdout', stdout);
-        console.log('stderr', stderr);
-        done();
-    });
-}
-
 before(function(done) {
     var dir = __dirname,
         cmd = [
             // 'cp ' + dir + '/ssh_kit_test_key ~/.ssh',
             // 'chmod 600 ~/.ssh/ssh_kit_test_key',
             'cat ' + dir + '/ssh_kit_test_key.pub >> ~/.ssh/authorized_keys',
-            'mv ~/.ssh/authorized_keys ~/.ssh/authorized_keys.tmp',
             'chmod 600 ~/.ssh/authorized_keys && ls -la ~/.ssh'
         ].join(' && ');
 
@@ -32,24 +20,6 @@ before(function(done) {
         console.log('stderr: ' + stderr);
         done();
     });
-});
-
-after(function() {
-    var cmd = [
-        'rm ~/.ssh/authorized_keys',
-        'mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys'
-    ];
-    exec(cmd, function(err, stdout, stderr) {
-        if (err) {
-            return console.error(err);
-        }
-
-        console.log('executing: ' + cmd);
-        console.log('stdour: ' + stdout);
-        console.log('stderr: ' + stderr);
-        done();
-    });
-
 });
 
 describe('ssh-kit', function() {
