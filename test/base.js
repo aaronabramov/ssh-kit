ae773afe1f8c141a87b13ef7deb36fe8f3f609dc
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-08-31 23:04:56 -0700

    temp authorized_keys

--- test/base.js
+++ test/base.js
@@ -1,22 +1,25 @@
 var SSH = require('../'),
     exec = require('child_process').exec;
 
-function run(cmd) {
+function run(cmd, done) {
     exec(cmd, function(err, stdout, stderr) {
+        err && done(err);
         console.log(cmd);
         console.log('err', err);
         console.log('stdout', stdout);
         console.log('stderr', stderr);
+        done();
     });
 }
 
 before(function(done) {
     var dir = __dirname,
         cmd = [
-            'cp ' + dir + '/ssh_kit_test_key /home/travis/.ssh',
-            'chmod 600 /home/travis/.ssh/ssh_kit_test_key',
-            'cat ' + dir + '/ssh_kit_test_key.pub >> /home/travis/.ssh/authorized_keys',
-            'chmod 600 /home/travis/.ssh/authorized_keys && ls -la /home/travis/.ssh'
+            // 'cp ' + dir + '/ssh_kit_test_key ~/.ssh',
+            // 'chmod 600 ~/.ssh/ssh_kit_test_key',
+            'cat ' + dir + '/ssh_kit_test_key.pub >> ~/.ssh/authorized_keys',
+            'mv ~/.ssh/authorized_keys ~/.ssh/authorized_keys.tmp',
+            'chmod 600 ~/.ssh/authorized_keys && ls -la ~/.ssh'
         ].join(' && ');
 
     exec(cmd, function(err, stdout, stderr) {
@@ -32,6 +35,25 @@ before(function(done) {
     });
 });
 
+after(function() {
+    var cmd = [
+        'rm ~/.ssh/authorized_keys',
+        'mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys'
+    ];
+    exec(cmd, function(err, stdout, stderr) {
+        if (err) {
+            console.err(err);
+            process.exit(1);
+        }
+
+        console.log('executing: ' + cmd);
+        console.log('stdour: ' + stdout);
+        console.log('stderr: ' + stderr);
+        done();
+    });
+
+});
+
 describe('ssh-kit', function() {
     beforeEach(function() {
         this.ssh = new SSH();
@@ -39,7 +61,7 @@ describe('ssh-kit', function() {
 
     it('connects to ssh', function(done) {
         this.ssh.set('host', 'localhost');
-        this.ssh.set('sshKey', '/home/travis/.ssh/ssh_kit_test_key');
+        this.ssh.set('sshKey', '~/.ssh/ssh_kit_test_key');
         this.ssh.exec('ls');
         this.ssh.on('finish', done);
     });
