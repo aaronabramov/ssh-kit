2b567d8fbb3d38f8624dc292a4aa1ec69c87c07a
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-09-02 08:41:13 -0700

    test setup

--- test/base.js
+++ test/base.js
@@ -1,28 +1,6 @@
 // ONLY USE ON CI BECAUSE IT MESSES UP `authorized_keys`
 
-var SSH = require('../'),
-    exec = require('child_process').exec;
-
-before(function(done) {
-    var dir = __dirname,
-        cmd = [
-            'cp ' + dir + '/ssh_kit_test_key ~/.ssh',
-            'chmod 600 ~/.ssh/ssh_kit_test_key',
-            'cat ' + dir + '/ssh_kit_test_key.pub >> ~/.ssh/authorized_keys',
-            'chmod 600 ~/.ssh/authorized_keys && ls -la ~/.ssh'
-        ].join(' && ');
-
-    exec(cmd, function(err, stdout, stderr) {
-        if (err) {
-            return console.error(err);
-        }
-
-        console.log('executing: ' + cmd);
-        console.log('stdour: ' + stdout);
-        console.log('stderr: ' + stderr);
-        done();
-    });
-});
+var SSH = require('../');
 
 describe('ssh-kit', function() {
     beforeEach(function() {
---
+++ test/setup.js
@@ -0,0 +1,22 @@
+var exec = require('child_process').exec;
+
+before(function(done) {
+    var dir = __dirname,
+        cmd = [
+            'cp ' + dir + '/ssh_kit_test_key ~/.ssh',
+            'chmod 600 ~/.ssh/ssh_kit_test_key',
+            'cat ' + dir + '/ssh_kit_test_key.pub >> ~/.ssh/authorized_keys',
+            'chmod 600 ~/.ssh/authorized_keys && ls -la ~/.ssh'
+        ].join(' && ');
+
+    exec(cmd, function(err, stdout, stderr) {
+        if (err) {
+            return console.error(err);
+        }
+
+        console.log('executing: ' + cmd);
+        console.log('stdour: ' + stdout);
+        console.log('stderr: ' + stderr);
+        done();
+    });
+});
